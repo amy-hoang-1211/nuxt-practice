d5164f8029f88a23a7f6ad3286ace444d4208ac2
Amy Hoang <amy.hoang@teamscal.com>
2020-12-03 15:45:37 +0700

    init project

---
+++ pages/fun.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="red">Hello World</h1>
+</template>
+
+<style>
+  .red {
+    color: red;
+  }
+</style>
+
+<script>
+  export default {
+    layout: 'blog'
+    // page component definitions
+  }
+</script>
